188d5e68413c60a9b782f1fd685c88a07093b2d4
Arthur Richard <devichard@gmail.com>
2025-09-28 20:11:00 -0300

    alteração link de repositório form (back)

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -27,7 +27,7 @@ export const ContactSection = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/ContactSection", {
+      const response = await fetch("https://myportfolio-2-bojl.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
