0b9e8ac0b5ec277182056656c92973c558e6a190
Arthur Richard <devichard@gmail.com>
2025-09-28 23:00:00 -0300

    new feature 2

--- backend-contact/server.js
+++ backend-contact/server.js
@@ -48,7 +48,9 @@ app.post("/ContactSection", async (req, res) => {
   try {
     // configura a rota do e-mail
     const transporter = nodemailer.createTransport({
-      service: "gmail", // ou outro SMTP
+      host: "smtp.gmail.com",
+      port: 587,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
